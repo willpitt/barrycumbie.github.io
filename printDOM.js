3942ca5c25855b60fb9ad1b99d86c550aa4c4209
willpitt <37159782+willpitt@users.noreply.github.com>
2018-03-07 16:01:53 -0600

    Update and rename printDOM to printDOM.js

--- printDOM.js
+++ printDOM.js
@@ -1,6 +1,8 @@
+console.log("Start");
 $(document).ready(function(){
 	var nodeList = document.body.childNodes;
 		for (i=0; i < nodeList.length; i++) {
 			console.log(document.body.childNodes[i]);
 		};
 };
+console.log("End");
